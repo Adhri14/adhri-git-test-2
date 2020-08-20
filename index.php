aaadf0a99966b99ff4af00449e1f14feeb223a01
adhri14 <adhri.adly@gmail.com>
2020-08-21 01:11:02 +0800

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
   <h3>Coba Git Remote</h3>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam ipsam eum odio repudiandae deleniti adipisci ipsum quod alias ab nostrum dicta cum quo voluptates quia, culpa cupiditate, excepturi cumque quas!</p>
 </body>
 </html>
